a0bad256170f31b49a301a5515404c31520881e7
mississppi <msspstudio@gmail.com>
2025-07-05 19:44:23 +0900

    setup

---
+++ 01-BlankWindow/BlankWindow/BlankWindow/AppDelegate.swift
@@ -0,0 +1,21 @@
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var window: NSWindow!
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        window = NSWindow(
+            contentRect: NSMakeRect(0, 0, 400, 300),
+            styleMask: [.titled, .closable, .resizable],
+            backing: .buffered,
+            defer: false
+        )
+        window.center()
+        window.title = "Blank Window"
+        window.makeKeyAndOrderFront(nil)
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+}
---
+++ 01-BlankWindow/BlankWindow/BlankWindowTests/BlankWindowTests.swift
@@ -0,0 +1,17 @@
+//
+//  BlankWindowTests.swift
+//  BlankWindowTests
+//
+//  Created by KOSUKE SAKURAI on 2025/07/05.
+//
+
+import Testing
+@testable import BlankWindow
+
+struct BlankWindowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
